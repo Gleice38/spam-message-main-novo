c1f5d3f675c18c265acb9b13b023d1a109484b6b
renata reis <renatabrito98@gmail.com>
2026-01-13 16:40:25 -0300

    feat: integrate contacts page with backend services

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://89.117.33.220:8000",
+  baseURL: "http://89.117.33.220:8000/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
