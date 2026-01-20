9c8dada68000d0436df9b46950ee832421de00b8
renata reis <renatabrito98@gmail.com>
2026-01-20 14:01:34 -0300

    Permite segmentação por nome ou ID de região e área acadêmica no backend (envio seguro e flexível).

--- app/services/campaign_service.py
+++ app/services/campaign_service.py
@@ -35,19 +35,21 @@ class CampaignService:
         from app.constants.data import REGIONS, ACADEMIC_AREAS
         if "regions" in filters:
             selected_states = []
-            for region_name in filters["regions"]:
-                for region in REGIONS:
-                    if region["name"] == region_name:
-                        selected_states.extend(region["states"])
+            for region in filters["regions"]:
+                # Aceita tanto nome quanto id
+                for reg in REGIONS:
+                    if reg["name"] == region or reg["id"] == region:
+                        selected_states.extend(reg["states"])
             filters["states"] = selected_states
 
         # Converter academic_areas para academic_area_ids se necessário
         if "academic_areas" in filters:
             selected_ids = []
-            for area_name in filters["academic_areas"]:
-                for area in ACADEMIC_AREAS:
-                    if area["name"] == area_name:
-                        selected_ids.append(area["id"])
+            for area in filters["academic_areas"]:
+                # Aceita tanto nome quanto id
+                for a in ACADEMIC_AREAS:
+                    if a["name"] == area or a["id"] == area:
+                        selected_ids.append(a["id"])
             filters["academic_area_ids"] = selected_ids
 
         contacts = contact_repo.get_by_filters(filters)
